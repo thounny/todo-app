ce66d883467fbea2bcb2b9a3d0b7a76697c0bfd2
thounny <thounnykeo@gmail.com>
2021-12-15 02:48:25 -0500

    Modified index.js == Completed todolist (simple build version)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//1. When new text is written into the input, its state should be saved.
-//2. When the add button is pressed, the current data in the input should be
-//added to an array.
-//3. The <ul> should display all the array items as <li>s
+//1. When new text is written into the input == state should be saved. [X]
+//2. When the add button is pressed, the current data in the input should be 
+//added to an array. [X]
+//3. The <ul> should display all the array items as <li> [X]
